41681ec91d3ecbd053d1fea74d4e4f5f6e4be6a3
payalrajput1781 <144619583+payalrajput1781@users.noreply.github.com>
2023-09-20 16:53:48 +0530

    Add files via upload

---
+++ Assignment Task 1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 20 15:31:58 2023
+
+@author: payal
+"""
+
+radius = 1.1
+area = 3.14 * (radius ** 2)
+print(f"The area of the circle with radius {radius} is: {area}")
