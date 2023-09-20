75aed6770d9ba6d9434035e2bb9a9bbbd5826a67
payalrajput1781 <144619583+payalrajput1781@users.noreply.github.com>
2023-09-20 17:58:36 +0530

    Add files via upload

---
+++ Assignment Task 2.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 20 17:14:47 2023
+
+@author: payal
+"""
+
+
+filename = input("Input the Filename:Assignment Task 1.py ")
+file_extension = filename.split(".")[-1]
+print(f"The extension of the file is: {file_extension}")
