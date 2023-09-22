e4dffbef579b68b981656aaa073cb8b8ef1b12ff
payalrajput1781 <144619583+payalrajput1781@users.noreply.github.com>
2023-09-22 18:22:23 +0530

    Add files via upload

---
+++ let's code loops (2).py
@@ -0,0 +1,15 @@
+list1 = [11, -21, 0, 45, 66, -93]
+num = 0
+while(num < len(list1)):
+    if list1[num] >= 0:
+        print(list1[num], end = " ")
+    num += 1      
+        
+    
+    
+list2 = [7, -8, 1, 2, 9, -6, -4]
+num = 0
+while(num < len(list2)):
+    if list2[num] >= 0:
+        print(list2[num], end = " ")
+    num += 1    
