c6600e901c7543fe519255f659f4a09819e3e2f8
payalrajput1781 <144619583+payalrajput1781@users.noreply.github.com>
2023-09-22 14:41:03 +0530

    Add files via upload

---
+++ Let's code Loops.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 22 13:29:28 2023
+
+@author: payal
+"""
+myList = [1, 2, 3, 5, 8, 13, 21]
+for number in myList:
+    print(number)
+
+
+
+
+i = 3
+while i <= 21:
+    print(i)
+    if i == 13:
+        break
+    i += 1
+
+
+
+
+age = 12
+if age > 18:
+    print("you can vote")
+else:
+    print("you cannot vote")
+
+       
+i = 8
+while i > 0:
+    i -= 1
+    if i == 3:
+        continue
+    print("value is:", i)
+    
+    
+cost = 500
+if cost>200:
+    print("s")
+elif cost>400:
+    print("T")
+else:
+    print("d")
+    
+         
+    
+        
+    
+    
